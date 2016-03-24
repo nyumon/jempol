09cbb8a13d496f84a0fdcc0b370136ddeefc7ca7
riskteria <riskteria@gmail.com>
2016-03-25 01:48:45 +0700

    Fixed Back Button and Toolbar  on New Post Activity

--- app/src/main/java/com/nyumon/jempol/NewPostActivity.java
+++ app/src/main/java/com/nyumon/jempol/NewPostActivity.java
@@ -12,14 +12,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-<<<<<<< HEAD
-import android.support.v7.widget.Toolbar;
-
-public class NewPostActivity extends AppCompatActivity {
-
-
-    private Toolbar toolbar;
-=======
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -35,14 +27,6 @@ public class NewPostActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
-    private int[] tabIcons = {
-            R.drawable.time1,
-            R.drawable.people1,
-            R.drawable.left1,
-            R.drawable.music1,
-            R.drawable.gear1
-    };
->>>>>>> 3c5ef4b7b7c63aaa0ffe9f7e5f638c4306d74afc
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,22 +35,14 @@ public class NewPostActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-<<<<<<< HEAD
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-=======
-
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-
 
         final ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(R.drawable.time1);
+        actionBar.setHomeAsUpIndicator(R.drawable.arrows);
 
->>>>>>> 3c5ef4b7b7c63aaa0ffe9f7e5f638c4306d74afc
     }
     @Override
     public boolean onOptionsItemSelected(final MenuItem item) {
@@ -80,20 +56,6 @@ public class NewPostActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    public void NewPost(View view){
-        Intent intent = new Intent(this, NewPostActivity.class);
-        startActivity(intent);
-    }
 
 
-
-    }
-
-
-
+}
