e4af36cf52d158bbe08492dc490e6a9f8a8a000d
rickyramadhann <rickyramadhan480@gmail.com>
2016-03-27 13:50:08 +0700

    Exit Alert

--- app/src/main/java/com/nyumon/jempol/LoginActivity.java
+++ app/src/main/java/com/nyumon/jempol/LoginActivity.java
@@ -4,10 +4,14 @@ package com.nyumon.jempol;
  * Created by fajar on 26/03/16.
  */
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import android.os.Handler;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -63,4 +67,41 @@ public class LoginActivity extends Activity {
             }
         });
     }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            AlertDialog.Builder alertbox = new AlertDialog.Builder(LoginActivity.this);
+
+            alertbox.setTitle("Do You Want To Exit ?");
+            alertbox.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface arg0, int arg1) {
+                    // finish used for destroyed activity
+                    exit();
+                }
+            });
+
+            alertbox.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface arg0, int arg1) {
+                    // Nothing will be happened when clicked on no button
+                    // of Dialog
+                }
+            });
+
+            alertbox.show();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    public void exit()
+    {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+
+
+
 }
